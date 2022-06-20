b94d2c69fa226b89157bfa467fec825a561c9a83
Houser97 <arturo.riverar97@gmail.com>
2022-06-20 10:13:08 -0500

    Create footer, style it and modify header and index css files
    Both the footer and header were given a different font-family.
    Furthermore, in the index.css file the rule box-sizing was defined
    to prevent the padding defined from expanding the current div's width

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header currentScore = {currentScore} bestScore = {bestScore}/>
+      <Footer />
     </div>
   );
 }
